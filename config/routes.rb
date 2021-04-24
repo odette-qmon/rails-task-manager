5264ac595a3210426d8955bfcc285b495e1dd281
Odette Quiroz <odette.qmon@gmail.com>
2021-04-24 14:45:23 -0500

    optionals missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  # The `new` route needs to be *before* `show` route.
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  # as: :edit_task --> works with edit_task_path(@task) = (@task.id) params del id
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
